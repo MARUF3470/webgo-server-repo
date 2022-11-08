8bf98e2d66902d1e505ec34792a666c8b8a178c9
MARUF3470 <smmaruf25@gmail.com>
2022-11-09 02:25:47 +0600

    service details api created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
@@ -19,15 +19,21 @@ async function run() {
     try {
         app.get('/threeServices', async (req, res) => {
             const query = {};
-            const cursor = webgoServiceCollecton.find(query)
-            const result = await cursor.limit(3).toArray()
-            res.send(result)
+            const cursor = webgoServiceCollecton.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
         })
         app.get('/services', async (req, res) => {
             const query = {}
-            const cursor = webgoServiceCollecton.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+            const cursor = webgoServiceCollecton.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const service = await webgoServiceCollecton.findOne(query);
+            res.send(service)
         })
     }
     finally {
