5e4cfa972c102018bdb75db106871dd866a338b0
MARUF3470 <smmaruf25@gmail.com>
2022-11-09 02:33:02 +0600

    secret document hide

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 
-//USER: webgoServicesDb
-//PASSWORD: gifnLhPIDP9EEw6b
-
-
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://webgoServicesDb:gifnLhPIDP9EEw6b@cluster0.z1xe9fw.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.z1xe9fw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
