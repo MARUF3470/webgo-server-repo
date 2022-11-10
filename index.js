2d7637293eacc51910a21e35377973a80c2217f8
MARUF3470 <smmaruf25@gmail.com>
2022-11-10 22:02:07 +0600

    jwt token added

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ const port = process.env.PORT || 5000;
 require('dotenv').config()
 app.use(cors())
 app.use(express.json())
+var jwt = require('jsonwebtoken');
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.z1xe9fw.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -15,6 +33,11 @@ async function run() {
     const webgoServiceCollecton = client.db('webgo').collection('services')
     const reviewCollection = client.db('webgo').collection('reviews')
     try {
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ token })
+        })
         app.post('/services', async (req, res) => {
             const data = req.body;
             const result = await webgoServiceCollecton.insertOne(data)
@@ -57,9 +80,9 @@ async function run() {
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.get('/review', async (req, res) => {
-            console.log(req.query)
-            const query = { email: req.query.email }
+        app.get('/review/:email', verifyJWT, async (req, res) => {
+            const email = req.params.email
+            const query = { email: email }
             const cursor = reviewCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
