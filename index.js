f3e16d9d5e31a091c837424b39e3ac18bc9a9053
MARUF3470 <smmaruf25@gmail.com>
2022-11-10 16:46:39 +0600

    spacific service api created

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await webgoServiceCollecton.findOne(query)
+            res.send(result)
+        })
+
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
